924f0666a5329f9ff82295b641b76ba1eeb50bd3
MMeskhi <mishameskhi1@gmail.com>
2022-11-30 12:17:54 +0400

    finished sidebar

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+//Sidebar
+const sidebarCats = document.querySelectorAll(".sidebar-menu-drp");
+
+sidebarCats.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const parentCat = btn.closest(".sidebar-menu-drp");
+    const catOpen = parentCat.querySelector(".sidebar-menu-drp-cat");
+    const catOpenToggle = parentCat.querySelector(".menu-cat");
+    const filterToggle = () => {
+      catOpen.classList.toggle("sidebar-menu-drp-cat-active");
+      catOpenToggle.classList.toggle("menu-cat-active");
+    };
+    filterToggle(btn);
+  });
+});
